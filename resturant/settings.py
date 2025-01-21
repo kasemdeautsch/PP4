3d10f6ef81a8b70d163707536039dec98555fd68
Kasem Al <xdow69x0@students.codeinstitute.net>
2025-01-21 17:02:29 +0000

    change secret key

--- resturant/settings.py
+++ resturant/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dav4di&vx%-t1746a*+(n3z*@#+#mdcm9o&(hr-qhs=0=%dqoe'
+# SECRET_KEY = 'django-insecure-dav4di&vx%-t1746a*+(n3z*@#+#mdcm9o&(hr-qhs=0=%dqoe'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
