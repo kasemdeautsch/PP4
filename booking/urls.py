97ae734ebec4af38947be22045153e22a6c2ec26
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-02-15 19:07:29 +0100

    add view to list reservations

--- booking/urls.py
+++ booking/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    #path('res/', views.ReservationList.as_view(), name='home'),
+    path('reservations/', views.reservation_list, name='reservations'),
     path('', views.Home.as_view(), name='home'),
     # path('reservation_create', views.ResevationCreateView.as_view(), name="create-booking")
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import ListView, CreateView,TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 #from django.http import HttpResponse
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+from django.contrib.auth.models import User
 from .models import Reservation
 
 # Create your views here.
@@ -14,10 +19,21 @@ class Home(TemplateView):
     template_name = "booking/index.html"
 
 
-"""
-def test(request):
-    return HttpResponse("Hello, Reservations")
-"""
+@login_required
+def reservation_list(request):
+
+    
+    #user = get_object_or_404(User, user=request.user)
+    reservations = Reservation.objects.all()
+            
+    return render(
+        request,
+        "booking/reservation_list.html",
+        {
+            "reservations": reservations,
+        },
+        
+    )
 
 # class ReservationListView(ListView):
 
@@ -37,12 +53,4 @@ class ReservationList(ListView):
 class ResevationCreateView(CreateView):
     model = Reservation
     fields = ["name", "date", "time", "time", "notes", "user"]
-"""
-
-"""""
-def index(request):
-    if request.method == "POST":
-        return HttpResponse("You must have POSTed something")
-    else:
-        return HttpResponse(request.method+' Ok')
 """
