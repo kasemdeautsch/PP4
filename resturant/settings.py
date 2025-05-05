eefd0b5a51b84df4f919b28227b2d6338501c1de
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-05-05 19:04:24 +0200

    undebug for deploying

--- resturant/settings.py
+++ resturant/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-kasemdeautsch-pp4-rol7zn3brnf.ws.codeinstitute-ide.net',
                  '127.0.0.1',
